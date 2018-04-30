59199c9cceaa00337ae19f316a6cc19e75bb2ce6
Chen Jiang <chenjiang.uom@gmail.com>
2018-04-30 23:49:37 +1000

    Add files via upload

---
+++ esp32_websocket_onSwift.ino
@@ -0,0 +1,48 @@
+#include <WiFi.h>
+#include <WebSocketServer.h>
+ 
+WiFiServer server(80);
+WebSocketServer webSocketServer;
+ 
+const char *ssid = "MyESP32AP";
+const char *password = "77777777";
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  delay(4000);
+ 
+  WiFi.softAP(ssid, password);
+  Serial.println(WiFi.softAPIP());
+ 
+  server.begin();
+  delay(100);
+}
+ 
+void loop() {
+ 
+  WiFiClient client = server.available();
+ 
+  if (client.connected() && webSocketServer.handshake(client)) {
+ 
+    String data;      
+ 
+    while (client.connected()) {
+ 
+      data = webSocketServer.getData();
+ 
+      if (data.length() > 0) {
+         Serial.println("Get data from iPhone: " + data);
+         webSocketServer.sendData(data);
+      }
+ 
+      delay(10); // Delay needed for receiving the data correctly
+   }
+ 
+   Serial.println("The client disconnected");
+   delay(100);
+  }
+ 
+  delay(100);
+}
